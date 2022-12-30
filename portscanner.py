5f96fb17e39f2338b9e2216bcbe755d4128814ef
X-tims <121512813+X-tims@users.noreply.github.com>
2022-12-30 11:14:33 +0100

    update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,53 +1,32 @@
 import socket
 import sys
 
+
+
+
 addr = ""
-port = ""
+port = 8080
 timeout = ""
-
-class Portscanner:
     
-    def getInputfrmcli():
-        if sys.argv[0].isalpha() = True:
-            addr = socket.gethostbyname(sys.argv[0])
-        else:
-            addr = sys.argv[0]
-        port = sys.argv[1]
-        timeout = sys.argv[2]
-            
-    
+class Portscanner: 
     def getInputinteractive():
-        if sys.argv[0].isalpha() = True:
-            addr = socket.gethostbyname(sys.argv[0])
+        addrs = input("[+] Address: ")
+        if addrs.isalpha == True:
+        	addr = socket.gethostbyname(addrs)
         else:
-            addr = sys.argv[0]
-        port = input("[+] Port: ")
+        	addr = addrs
+       # port = int(input("[+] Port: "))
         timeout = input("[+] Timeout(s): ")
-        
-    
-    def connect():
-        if __name__ == "__main__":
-            print(">> Initiating....")
-            getInputinteractive()
-            print(">>> Instantiating connection...")
-            sock = socket.create_connection((addr,port))
-            print("✓ Connected")
-            socket.setdefaulttimeout(int(timeout)
-        else:
-            print(">> Initiating....")
-            getInputfrmcli()
-            print(">>> Instantiating connection...")
-            sock = socket.create_connection((addr,port))
-            print("✓ Connected")
-            socket.setdefaulttimeout(int(timeout)
-    def testconn():
-        sock = connect()
-        while sock = True:
-            sock.send(b"TCK")
-            sock.recv(1024).decode()
-        else:
-            pass
-            
-           
 
-testconn()
+    def connect():
+            print(">> Initiating....")
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+            
+            Portscanner.getInputinteractive()
+            print(">>> Instantiating connection...")
+            print(port)
+            sock.connect((addr, port))
+            print("✓ Connected")
+            socket.setdefaulttimeout(int(timeout))
+            
+Portscanner.connect()
