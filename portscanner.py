704ccb36525f862eefce0b2a70133c7d0231b094
X-tims <121512813+X-tims@users.noreply.github.com>
2022-12-30 09:33:27 +0100

    update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,2 +1,17 @@
 import os
-import socket 
+import socket
+import Ipy
+import sys
+class Portscanner:
+    def __init__(self, getInput()):
+        self.getInput() = getInput()
+        
+    def getInputfrmcli(addr, port, timeout):
+        sys.args[0] = addr
+        sys.args[1] = port
+        sys.args[2] = timeout
+    
+    def getInputinteractive(addr, port, timeout):
+        
+
+
