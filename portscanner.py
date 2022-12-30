9489b58eb1390d0b0e32ec39dc4457741628ee29
X-tims <121512813+X-tims@users.noreply.github.com>
2022-12-30 09:40:13 +0100

    update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -3,15 +3,18 @@ import socket
 import Ipy
 import sys
 class Portscanner:
-    def __init__(self, getInput()):
-        self.getInput() = getInput()
-        
-    def getInputfrmcli(addr, port, timeout):
-        sys.args[0] = addr
-        sys.args[1] = port
-        sys.args[2] = timeout
-    
-    def getInputinteractive(addr, port, timeout):
-        
+def __init__(self, sockettype, recvamt):
+    self.sockettype = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+    self.recvamt = 1024 * 1024
 
+def getInputfrmcli(addr, port, timeout):
+sys.args[0] = addr
+sys.args[1] = port
+sys.args[2] = timeout
 
+def getInputinteractive(addr, port, timeout):
+    addr = input("[+] Address: ")
+    port = input("[+] Port: ")
+    timeout = input("[+] Timeout(s): ")
+
+def 
