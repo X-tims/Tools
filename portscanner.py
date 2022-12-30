7dad8ae331593be94a07b324a4bcf842a1f8e81b
X-tims <121512813+X-tims@users.noreply.github.com>
2022-12-30 10:03:05 +0100

    update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -2,21 +2,33 @@ import os
 import socket
 import Ipy
 import sys
+
+addr = ""
+port = ""
+timeout = ""
+
 class Portscanner:
-def __init__(self, sockettype, recvamt):
-self.sockettype = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto = 0)
-self.recvamt = 1024 * 1024
-
-def getInputfrmcli(self):
-    addr= sys.args[0]
-    port = sys.args[1]
-    timeout = sys.args[2]
-
-def getInputinteractive(self):
-    addr = input("[+] Address: ")
-    port = input("[+] Port: ")
-    timeout = input("[+] Timeout(s): ")
-
-def connect(self):
-    if __name__ == "__main__":
-        getInputinteractive()
+    def __init__(self, recvamt):
+        self.recvamt = 1024 * 1024
+    
+    def getInputfrmcli(self):
+        addr= sys.args[0]
+        port = sys.args[1]
+        timeout = sys.args[2]
+    
+    def getInputinteractive(self):
+        addr = input("[+] Address: ")
+        port = input("[+] Port: ")
+        timeout = input("[+] Timeout(s): ")
+    
+    def connect(self):
+        if __name__ == "__main__":
+            getInputinteractive()
+            socket.create_connection((addr,port))
+            socket.setdefaulttimeout(int(timeout)
+        else:
+            getInputfrmcli()
+            socket.create_connection((addr,port))
+            socket.setdefaulttimeout(int(timeout)
+        
+        
